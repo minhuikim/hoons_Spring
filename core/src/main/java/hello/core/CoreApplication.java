4d9b5eb5d7ee2ce67d49d710d8f1f0c52f50ada6
HOON <nice130@naver.com>
2023-05-10 23:52:47 +0900

    설정오류 재생성하자

--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -1,4 +1,4 @@
-package hello.core;
+package main.java.hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,4 @@
+package main.java.hello.core.discount;
+
+public class DiscountPolicy {
+}
